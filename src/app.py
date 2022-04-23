6ffcc50e97c8937452c4e5144d511b0a6d99c039
irofessional <20381860+irofessional@users.noreply.github.com>
2022-04-23 16:18:50 +0900

    BOT本体を追加

--- src/app.py
+++ src/app.py
@@ -1,9 +1,7 @@
-import json
 import os
 import re
 
 import discord
-from requests import request
 import requests
 
 # os.getenv(key="DISCORD_TOKEN")
@@ -15,7 +13,7 @@ MUSICLINK_ENDPOIT = "https://api.song.link/v1-alpha.1/links"
 SPOTIFY_URL_REGEX = re.compile(
     r'https?://open.spotify.com/(track|album|artist)/[\w]+')
 APPLEMUSIC_URL_REGEX = re.compile(
-    r'https?://music\.apple\.com/jp/(album|artist)/.+/[\d]+')
+    r'https?://music\.apple\.com/jp/(album|artist)/[\w\-\%]+/[\d]+')
 
 
 class MusicLinkConverter(discord.Bot):
@@ -24,10 +22,46 @@ class MusicLinkConverter(discord.Bot):
             return
 
         if message.channel.category_id == DISOCRD_CATEGORY_ID:
-            mtc = re.search(SPOTIFY_URL_REGEX, message.content)
+            mtc = re.search(APPLEMUSIC_URL_REGEX, message.content)
+            if mtc is None:
+                return
+
             music_url: str = mtc.group()
-            req = requests.get(MUSICLINK_ENDPOIT, params={"url": music_url})
-            print(req.json())
+            req = requests.get(MUSICLINK_ENDPOIT, params={
+                               "url": music_url, "userCountry": "JP"})
+            try:
+                req.raise_for_status()
+            except Exception:
+                return
+            music_data: dict = req.json()
+            spotify_url: str
+            applemusic_url: str
+
+            try:
+                spotify_url = music_data['linksByPlatform']['spotify']['url']
+            except KeyError:
+                pass
+            try:
+                applemusic_url = applemusic_url = music_data["linksByPlatform"]["appleMusic"]["url"]
+            except KeyError:
+                pass
+
+            view = discord.ui.View()
+
+            if spotify_url is None and applemusic_url is None:
+                return
+
+            if spotify_url is not None:
+                spotify_btn = discord.ui.Button(
+                    style=discord.ButtonStyle.link, label="Spotify", url=spotify_url)
+                view.add_item(spotify_btn)
+
+            if applemusic_url is not None:
+                applemusic_btn = discord.ui.Button(
+                    style=discord.ButtonStyle.url, label="Apple Music", url=applemusic_url)
+                view.add_item(applemusic_btn)
+
+            await message.reply(view=view)
 
 
 intents = discord.Intents.default()
