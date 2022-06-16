f250a24b4c99632d371ac70d5ad26cd0ed7ae5bd
irofessional <20381860+scarlet0725@users.noreply.github.com>
2022-06-16 16:18:32 +0900

    Add Message Paser

---
+++ src/userMessagePaser.ts
@@ -0,0 +1,21 @@
+import { encode } from "querystring"
+
+export const userMessagePaser = (messageText: string): string | null => {
+    const urlRegexList = [
+        /(https?:\/\/music\.apple\.com\/jp\/album\/[\w\-\%]+\/[\d]+)/,
+        /(https?:\/\/music\.apple\.com\/jp\/artist\/[\w\-\%]+\/[\d]+)/,
+        /(https?:\/\/open\.spotify\.com\/album\/[\w]+)/,
+        /(https?:\/\/open\.spotify\.com\/artist\/[\w]+)/,
+        /(https?:\/\/open\.spotify\.com\/track\/[\w]+)/,
+    ]
+
+    for (const element of urlRegexList) {
+        const match = messageText.match(element)
+        if (match) {
+            return match[0]
+        }
+    }
+
+    return null
+    
+}
